6e538d9eae8acc666970161ac0337f316ab856b7
feden2906 <feden2906@gmail.com>
2022-10-05 13:36:28 +0300

    kk

--- configs/config.js
+++ configs/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'production',
-
+  BACKAND_URL: process.env.BACKAND_URL || 'http://localhost:5000',
   PORT: process.env.PORT || 5000,
 };
--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,14 +1,16 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+const { BACKAND_URL } = require('../configs/config');
+
 module.exports = {
   getImage: async (req, res, next) => {
     try {
       const filesNames = await fs.readdir(path.join(process.cwd(), 'public'));
-      console.log('$$$$$$',`${req.headers['x-forwarded-proto'] || req.connection.info.protocol}://${req.info.host}${req.url.path}`);
+
       const randomName = filesNames[Math.floor(Math.random() * filesNames.length)];
 
-      res.json(`http://localhost:5000/static/${randomName}`);
+      res.json(`${BACKAND_URL}/static/${randomName}`);
     } catch (e) {
       next(e);
     }
